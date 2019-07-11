f60532d18a3355da0c5e52c21cb340f911d8aef3
renxinwei <413038044@qq.com>
2019-07-12 03:45:37 +0800

    1

---
+++ libs/jquery.extend.giveup.js
@@ -0,0 +1,25 @@
+$.prototype.giveup_attrs=function(attr){
+    var ss = [];
+    $(this).each(function(index,ele){
+        ss.push($(ele).attr(attr))
+    })
+    return ss
+}
+
+$.prototype.giveup_exchangeDom=function(target){
+    debugger
+    if(!target||target.length == 0)
+        return
+   var s=$('<span></span>')
+    s.insertAfter(this)
+
+    var s1=$('<span></span>')
+    s1.insertAfter(target)
+
+    $(target).insertBefore(s)
+    $(this).insertAfter(s1)
+
+    s.remove()
+    s1.remove()
+
+}
---
+++ maintainer/libs/public.js
@@ -0,0 +1,16 @@
+
+
+function resCodeProcess(code,codeMsg) {
+    debugger
+    var win = window.parent !== window ? window.parent : window
+
+    if (code == 20) {
+        alert(codeMsg)
+        win.location.href=rootDis+'maintainer/login.html'
+    }
+    else if (code == 98) {
+
+    } else
+        alert(codeMsg)
+
+}
