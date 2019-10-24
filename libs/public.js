c1366eb11f8f3064539325b1596fb8ed273ab7df
renxinwei <413038044@qq.com>
2019-10-24 12:46:39 +0800

    1

--- libs/public.js
+++ libs/public.js
@@ -1,8 +1,58 @@
 
-var public={}
+var public = {}
 
-public.toEmpty=function(value){
-    if(value === null || value=== undefined)
+public.toEmpty = function (value) {
+    if (value === null || value === undefined)
         return ''
     return value
+}
+
+public.subOmitRight = function (value, length) {
+    if (!value)
+        return ''
+    var s = ''
+    if (value.length > length)
+        s = '...'
+    return value.substring(0, length) + s
+}
+
+
+
+public.prettyFileSize = function (size) {
+    if (!size)
+        return "";
+
+    var num = 1024.00; //byte
+
+    if (size < num)
+        return size + "B";
+    if (size < Math.pow(num, 2))
+        return (size / num).toFixed(2) + "KB"; //kB
+    if (size < Math.pow(num, 3))
+        return (size / Math.pow(num, 2)).toFixed(2) + "MB"; //M
+    if (size < Math.pow(num, 4))
+        return (size / Math.pow(num, 3)).toFixed(2) + "GB"; //G
+    return (size / Math.pow(num, 4)).toFixed(2) + "TB"; //T
+}
+
+
+public.queryStringObject = function(queryString) {
+    if (!queryString)
+        queryString = window.location.search.substr(1);
+    if (queryString.startsWith('?'))
+        queryString.substr(1)
+    const queryList = queryString.split('&')
+    let result = {}
+    queryString && queryList.map((item) => {
+        let keyValue = item.split('=')
+        result[keyValue[0]] = decodeURIComponent(keyValue[1])
+    })
+    return result
+}
+
+
+public.getQueryStringParam = function(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
 }
--- maintain/libs/public.js
+++ maintain/libs/public.js
@@ -1,13 +1,73 @@
+var maintainPublic = {}
 
-function resCodeProcess(code,codeMsg) {
+maintainPublic.resCodeProcess = function (code, codeMsg) {
     debugger
-    var win = window.parent !== window ? window.parent : window
     if (code == 20) {
-        if(confirm(codeMsg)){
-            win.location.href='/maintain/login.html'
+        if (confirm('现在去登录>>>')) {
+            $(`<a href="/maintain/login.html"  >123</a>`)[0].click()
         }
     }
 
+}
+
+
+maintainPublic.newTab = function (title, url) {
+    parent.$('#indexPad-tabPad').tabs('add', {
+        title: title,
+        closable: true,
+        width: '100%',
+        content: `<iframe src="${url}" style="width:100%;height:100%;border:none;"/>`,
+        tools: [{
+            iconCls: 'icon-mini-refresh',
+            handler: function () {
+                var current_tab = parent.$('#indexPad-tabPad').tabs('getSelected');
+                parent.$('#indexPad-tabPad').tabs('update', {
+                    tab: current_tab,
+                    options: {
+                        content: current_tab.panel('options', 'content'),
+                    }
+                });
+            }
+        }
+        ]
+    });
+}
+
+
+maintainPublic.uploadImage = function (inputDom, ook) {
+    var r = prompt('已选的图片大小' + public.prettyFileSize(inputDom.files[0].size) + '，如需压缩，请输入质量1-9，取消则直接上传。', '8')
+    var compressIs = false
+    if (r != null) {
+        r = parseInt(r);
+        if (isNaN(r) || r < 1 || r > 9) {
+            alert('输入有误')
+            return;
+        }
+        compressIs = true
+    }
+    var file = inputDom.files[0];
+    var fd = new FormData()
+    fd.append('file', file);
+    $.ajax({
+        url: '/imgupload?' + $.param({ quality: r }),
+        type: 'POST',
+        data: fd,
+        cache: false,
+        processData: false,
+        contentType: false,
+        async: true,
+        success: function (res) {
+            if (res.codeMsg)
+                alert(res.codeMsg)
+            if (res.code != 0) {
+                maintainPublic.resCodeProcess(res.code, res.codeMsg)
+            } else {
+                if (compressIs)
+                    alert('压缩后文件大小' + public.prettyFileSize(inputDom.files[0].size))
+                ook(res.data.url)
+            }
+        }
+    })
 }
 
 
