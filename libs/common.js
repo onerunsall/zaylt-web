333f326630d3aad02ca9417bca5f328ae683d1a6
renxinwei <413038044@qq.com>
2019-11-28 10:31:54 +0800

    1

--- libs/common.js
+++ libs/common.js
@@ -48,3 +48,66 @@ common.queryStringObject = function(queryString) {
 
 
 
+
+common.uploadImage = function (inputDom, ook) {
+    var r = prompt('已选的图片大小' + common.prettyFileSize(inputDom.files[0].size) + '，如需压缩，请输入质量0-10，取消则直接上传。', '8')
+    var compressIs = false
+    if (r != null) {
+        r = parseInt(r);
+        if (isNaN(r) || r < 0 || r > 10) {
+            alert('输入有误')
+            return;
+        }
+        compressIs = true
+    }
+    var file = inputDom.files[0];
+    var fd = new FormData()
+    fd.append('file', file);
+    $.ajax({
+        url: '/imgupload?' + $.param({ quality: r }),
+        type: 'POST',
+        data: fd,
+        cache: false,
+        processData: false,
+        contentType: false,
+        async: true,
+        success: function (res) {
+            if (res.codeMsg)
+                alert(res.codeMsg)
+            if (res.code == 0) {
+                if (compressIs)
+                    alert('压缩后文件大小' + common.prettyFileSize(res.data.size))
+                ook(res.data.url)
+            } else {
+                
+            }
+        }
+    })
+}
+
+
+common.uploadFile = function (inputDom, ook) {
+    if (!confirm('已选的文件大小' + common.prettyFileSize(inputDom.files[0].size)))
+        return
+    var file = inputDom.files[0];
+    var fd = new FormData()
+    fd.append('file', file);
+    $.ajax({
+        url: '/fileupload',
+        type: 'POST',
+        data: fd,
+        cache: false,
+        processData: false,
+        contentType: false,
+        async: true,
+        success: function (res) {
+            if (res.codeMsg)
+                alert(res.codeMsg)
+            if (res.code == 0) {
+                ook(res.data.url)
+            } else {
+                
+            }
+        }
+    })
+}
--- maintain/libs/common.js
+++ maintain/libs/common.js
@@ -38,43 +38,5 @@ maintainCommon.newTab = function (title, url) {
 }
 
 
-maintainCommon.uploadImage = function (inputDom, ook) {
-    var r = prompt('已选的图片大小' + common.prettyFileSize(inputDom.files[0].size) + '，如需压缩，请输入质量0-10，取消则直接上传。', '8')
-    var compressIs = false
-    if (r != null) {
-        r = parseInt(r);
-        if (isNaN(r) || r < 0 || r > 10) {
-            alert('输入有误')
-            return;
-        }
-        compressIs = true
-    }
-    var file = inputDom.files[0];
-    var fd = new FormData()
-    fd.append('file', file);
-    $.ajax({
-        url: '/imgupload?' + $.param({ quality: r }),
-        type: 'POST',
-        data: fd,
-        cache: false,
-        processData: false,
-        contentType: false,
-        async: true,
-        success: function (res) {
-            if (res.codeMsg)
-                alert(res.codeMsg)
-            if (res.code != 0) {
-                maintainCommon.resCodeProcess(res.code, res.codeMsg)
-            } else {
-                if (compressIs)
-                    alert('压缩后文件大小' + common.prettyFileSize(res.data.size))
-                ook(res.data.url)
-            }
-        }
-    })
-}
-
-
-
 
 
